bc99e2878b26b26fee1660e6f14898706dc4609f
Juan Bolaños <juan.bolanos@epn.edu.ec>
2020-11-30 13:34:34 -0500

    Add sumOFArray inside Main Program

--- src/main/java/playerRegister/MainProgram.java
+++ src/main/java/playerRegister/MainProgram.java
@@ -67,6 +67,22 @@ public class MainProgram extends Application {
         //System.out.println(playerRegister.getAllUserIDs());
         //System.out.println(playerRegister.toString());
     }
+public class SumOfArray {
+
+    public static void main(String[] args) {
+        // You can try the method here
+        int[] array = {5, 1, 3, 4, 2};
+        System.out.println(sumOfNumbersInArray(array));
+    }
+
+    public static int sumOfNumbersInArray(int[] array) {
+        int suma = 0;
+        for (int aux : array) {
+            suma += aux;
+        }
+        return suma;
+    }
+}
 
 //    public static void print(PlayerRegister toPrint) {
 //
