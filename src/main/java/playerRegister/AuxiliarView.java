73321cfb879c9d55a4d1ac1650288a97c674b275
Juan Bolaños <juan.bolanos@epn.edu.ec>
2020-11-30 13:31:04 -0500

    Remove coments innecesary

--- src/main/java/playerRegister/AuxiliarView.java
+++ src/main/java/playerRegister/AuxiliarView.java
@@ -1,22 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package playerRegister;
 
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 
-<<<<<<< HEAD
 
-//ESTA CLASE ERA SOLO PARA PROBAR EL CAMBIO DE PANTALLA CON EL BOTÓN
-=======
-/**
- *
- * @author Ismael
- */
+
 
 //ESTA CLASE NO DEBE IR EN EL PROYECTO, ES AUXILIAR
 >>>>>>> main
